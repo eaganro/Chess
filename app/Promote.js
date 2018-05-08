e34372b07485a51e207afa5179b6af1b380f156c
Rory Eagan <rory.eagan@gmail.com>
2018-05-08 15:24:16 -0400

    Adding promote file for later use.

---
+++ app/Promote.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Promote extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          Queen
+        </div>
+        <div>
+          Rook
+        </div>
+        <div>
+          Knight
+        </div>
+      </div>
+    );
+  }
+}
